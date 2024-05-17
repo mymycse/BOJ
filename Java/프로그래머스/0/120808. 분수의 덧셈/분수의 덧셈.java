992569a48c08551ae8c249924f0145cf2bcbc888
mymycse <162075734+mymycse@users.noreply.github.com>
2024-05-18 08:44:52 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.10 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        int[] answer = {numer1*denom2 + numer2*denom1, denom1*denom2};
+        
+        for ( int i = answer[1] ; i > 1 ; i-- ) {
+            if ( answer[0]%i == 0 && answer[1]%i == 0 ) {
+                answer[0] /= i;
+                answer[1] /= i;
+            }
+        }
+        return answer;
+    }
+}
