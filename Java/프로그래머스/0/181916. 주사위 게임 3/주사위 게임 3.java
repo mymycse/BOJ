b166148d0aeec774f7ab2ba57fe5a132395fd091
mymycse <162075734+mymycse@users.noreply.github.com>
2024-05-23 19:21:39 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.42 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int a, int b, int c, int d) {
+        int answer = 0;
+        int[] arr = {a,b,c,d};
+        Arrays.sort(arr);
+        
+        if ( arr[0] == arr[3] )
+            answer = arr[0]*1111;
+        else if ( arr[0] == arr[2] )
+            answer = (int)Math.pow(10*arr[0]+arr[3],2);
+        else if ( arr[1] == arr[3] )
+            answer = (int)Math.pow(10*arr[3]+arr[0],2);
+        else if ( arr[0] == arr[1] && arr[2] == arr[3] )
+            answer = (arr[0]+arr[2])*Math.abs(arr[0]-arr[2]);
+        else if ( arr[0] == arr[1] )
+            answer = arr[2]*arr[3];
+        else if ( arr[1] == arr[2] )
+            answer = arr[0]*arr[3];
+        else if ( arr[2] == arr[3] )
+            answer = arr[0]*arr[1];
+        else 
+            answer = arr[0];
+        
+        return answer;
+    }
+}
