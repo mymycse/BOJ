79465fb839f6fd42859de545d27611d480d1b49d
mymycse <162075734+mymycse@users.noreply.github.com>
2024-05-30 10:21:03 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.03 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        
+        if ( num_list.length > 10 ) {
+            for ( int num : num_list )
+                answer += num;
+        } else {
+            answer++;
+            for ( int num : num_list )
+                answer *= num;
+        }
+        
+        return answer;
+    }
+}
