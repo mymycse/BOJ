90636f23758457c016b91111dfa305ee95834367
my <162075734+mymycse@users.noreply.github.com>
2024-07-08 23:19:09 +0900

    [Bronze IV] Title: Identifying tea, Time: 208 ms, Memory: 17804 KB -BaekjoonHub

---
+++ Java/백준/Bronze/11549. Identifying tea/Identifying tea.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int cnt = 0;
+
+        for ( int i = 0 ; i < 5 ; i++ )
+            if ( t == sc.nextInt() )
+                cnt++;
+
+        System.out.println(cnt);
+        sc.close();
+    }
+}
