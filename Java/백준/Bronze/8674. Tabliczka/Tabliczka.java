53bfdce30aef4a918175ea7b5c35dcf353390182
my <162075734+mymycse@users.noreply.github.com>
2024-08-15 23:07:50 +0900

    [Bronze IV] Title: Tabliczka, Time: 164 ms, Memory: 17576 KB -BaekjoonHub

---
+++ Java/백준/Bronze/8674. Tabliczka/Tabliczka.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+
+        if ( a%2 == 0 || b%2 == 0 )
+            System.out.println(0);
+        else
+            System.out.println(Math.min(a,b));
+    }
+}
