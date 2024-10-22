32a09a47e292f9be0671e9ebba9265a4a876f68c
my <162075734+mymycse@users.noreply.github.com>
2024-10-22 13:16:39 +0900

    [Silver III] Title: N과 M (7), Time: 576 ms, Memory: 123636 KB -BaekjoonHub

---
+++ Java/백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] arr, res;
+    static StringBuilder sb = new StringBuilder();
+    static void dfs( int len ) {
+        if ( len == M ) {
+            for ( int val : res )
+                sb.append(val).append(" ");
+            sb.append("\n");
+            return;
+        }
+
+        for ( int i = 0 ; i < N ; i++ ) {
+            res[len] = arr[i];
+            dfs(len+1);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        res = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for ( int i = 0 ; i < N ; i++ )
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        dfs(0);
+
+        System.out.print(sb);
+    }
+}
