2e9ea9ef22f98dafe1b66b2bc59bd0cf545bc263
Minyoung Cho <162075734+mymycse@users.noreply.github.com>
2025-04-02 19:51:59 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll N;
+    cin >> N;
+
+    cout << N*(N-1)*(N-2)/6 << '\n' << 3;
+    return 0;
+}
