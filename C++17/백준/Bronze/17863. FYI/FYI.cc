d99bc7f0e12f82783d3c04bd60cc0b5223d060df
Minyoung Cho <162075734+mymycse@users.noreply.github.com>
2025-04-16 22:46:44 +0900

    [Bronze IV] Title: FYI, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/17863. FYI/FYI.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int N;
+    cin >> N;
+    
+    if ( N/10000 == 555 ) cout << "YES";
+    else cout << "NO";
+    
+    return 0;
+}
