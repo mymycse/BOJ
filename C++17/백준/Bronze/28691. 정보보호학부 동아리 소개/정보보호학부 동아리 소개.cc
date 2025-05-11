2e1706c0a8289fd4fed9e996147aef80e2079208
Minyoung Cho <162075734+mymycse@users.noreply.github.com>
2025-05-11 23:34:12 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0), cin.tie(0);
+    char c;
+    cin >> c;
+    
+    if ( c == 'M' ) cout << "MatKor";
+    else if ( c == 'W' ) cout << "WiCys";
+    else if ( c == 'C' ) cout << "CyKor";
+    else if ( c == 'A' ) cout << "AlKor";
+    else cout << "$clear";
+    
+    return 0;
+}
