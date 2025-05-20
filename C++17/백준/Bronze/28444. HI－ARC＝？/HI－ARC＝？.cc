614187dad54e4020c105c64bcd0c108f25b7b3f5
Minyoung Cho <162075734+mymycse@users.noreply.github.com>
2025-05-20 23:46:45 +0900

    [Bronze V] Title: HI-ARC=?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/28444. HI－ARC＝？/HI－ARC＝？.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    int h,i,r,a,c;
+    cin >> h >> i >> r >> a >> c;
+    cout << h*i-r*a*c;
+    return 0;
+}
