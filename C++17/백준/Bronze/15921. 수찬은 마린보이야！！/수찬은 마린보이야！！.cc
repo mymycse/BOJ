da6cf29587f08355ea2602918b7e126f2d966391
Minyoung Cho <162075734+mymycse@users.noreply.github.com>
2025-05-24 22:03:28 +0900

    [Bronze III] Title: 수찬은 마린보이야!!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/15921. 수찬은 마린보이야！！/수찬은 마린보이야！！.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    
+    int n;
+    cin >> n;
+    
+    if ( n == 0 )
+        cout << "divide by zero";
+    else
+        cout << "1.00";
+    return 0;
+}
