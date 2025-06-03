24ac89d9db5b4cd9e5133d4d56b161aa5bbd73df
Minyoung Cho <162075734+mymycse@users.noreply.github.com>
2025-06-03 23:58:27 +0900

    [Bronze V] Title: 果物 (Fruit), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/31606. 果物 （Fruit）/果物 （Fruit）.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    
+    int n, m, sum = 3;
+    cin >> n >> m;
+    
+    sum = sum + n + m;
+    cout << sum;
+    
+    return 0;
+}
