58ce22dfb6f3ddc23a4bfe08147fefb46ef65a1a
Minyoung Cho <162075734+mymycse@users.noreply.github.com>
2025-06-08 23:25:42 +0900

    [Bronze V] Title: Conveyor Belt Sushi, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/32326. Conveyor Belt Sushi/Conveyor Belt Sushi.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << a*3 + b*4 + c*5;
+    return 0;
+}
