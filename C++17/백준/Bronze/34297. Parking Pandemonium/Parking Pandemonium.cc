3bc201583908f1bc41d22e641abb7dd552daceec
Minyoung Cho <162075734+mymycse@users.noreply.github.com>
2025-09-22 23:54:13 +0900

    [Bronze V] Title: Parking Pandemonium, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/34297. Parking Pandemonium/Parking Pandemonium.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long M, K, C;
+    cin >> M >> K >> C;
+    cout << M * C << '\n';
+    return 0;
+}
