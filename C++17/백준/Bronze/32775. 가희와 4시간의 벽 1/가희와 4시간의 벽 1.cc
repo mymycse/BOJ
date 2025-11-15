70db0151ce33e0e584b11a249502d59d37604223
Minyoung Cho <162075734+mymycse@users.noreply.github.com>
2025-11-15 12:43:29 +0900

    [Bronze V] Title: 가희와 4시간의 벽 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/32775. 가희와 4시간의 벽 1/가희와 4시간의 벽 1.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    int S, F;
+    cin >> S >> F;
+    
+    if ( S > F ) cout << "flight";
+    else cout << "high speed rail";
+}
